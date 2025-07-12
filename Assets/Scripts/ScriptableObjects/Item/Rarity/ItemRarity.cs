48f0a4ec27c0164d8656cbe2936f57b722040b64
VeteranXT <Vladanlikodric@hotmail.com>
2025-07-12 11:08:13 +0200

    a

--- Assets/Scripts/ScriptableObjects/Item/Rarity/ItemRarity.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(menuName = "Customs/New Item Rarity")]
-public class ItemRarity : ScriptableObject
-{
-    public string rarity;
-    [Expose("Rarity Color")]
-    public Color rarityColor;
-}
